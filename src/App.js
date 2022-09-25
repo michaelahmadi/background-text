3f62ffb35c662f63cfc481ed5e34475ef36d2ab4
Michael Ahmadi <53944364+michaelahmadi@users.noreply.github.com>
2022-09-25 18:56:20 -0500

    Committing progress on resolution dropdown, not fully functional yet

--- src/App.js
+++ src/App.js
@@ -3,18 +3,43 @@ import { useEffect, useState, useRef } from 'react';
 function App() {
 
   const [backgroundText, setBackgroundText] = useState(""); // The inputted text
+  const [dimensions, setDimensions] = useState({width: 1920, height: 1080});
   const [font, setFont] = useState(54);
 
-  const TWO_THIRDS = 2/3;
+  const DimensionDropdown = props => {
+    const options = [
+      { label: '1920x1080', value: '{"width": 1920, "height": 1080}'},
+      { label: '1440x900', value: '{"width": 1440, "height": 900}' },
+      { label: '1366x768', value: '{"width": 1366, "height": 768}' },
+    ];
+
+    const handleChange = (e) => {
+      setDimensions(JSON.parse(e.target.value));
+    }
+
+    return (
+      <div>
+        <label>
+          Screen Resolution: 
+          <select value={dimensions} onChange={handleChange}>
+            {options.map((option) => (
+              <option key={option.label} value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        </label>
+      </div>
+    )
+  }
 
   const DisplayCanvas = props => {
+    const TWO_THIRDS = 2/3;
 
     const canvasRef = useRef(null);
     
     useEffect(() => {
       const ctx = canvasRef.current.getContext('2d');
-      const scaledWidth = props.width * TWO_THIRDS;
-      const scaledHeight = props.height * TWO_THIRDS;
+      const scaledWidth = dimensions.width * TWO_THIRDS;
+      const scaledHeight = dimensions.height * TWO_THIRDS;
       const scaledFont = props.font * TWO_THIRDS;
       // Draw background
       ctx.fillStyle = "white";
@@ -25,9 +50,9 @@ function App() {
       ctx.fillStyle = 'black';
       ctx.textAlign = 'center';
       ctx.fillText(backgroundText, scaledWidth/2, scaledHeight/2);
-    }, [props]);
+    }, [props, TWO_THIRDS]);
 
-    return <canvas ref={canvasRef} width={props.width * TWO_THIRDS} height={props.height * TWO_THIRDS} style={{border: '1px solid black'}}>Your browser does not support the canvas HTML element.</canvas>;
+    return <canvas ref={canvasRef} width={dimensions.width * TWO_THIRDS} height={dimensions.height * TWO_THIRDS} style={{border: '1px solid black'}}>Your browser does not support the canvas HTML element.</canvas>;
   };
  
   const DownloadBackground = props => {
@@ -44,13 +69,13 @@ function App() {
       const ctx = canvasRef.current.getContext('2d');
       // Draw background
       ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, 1920, 1080);
+      ctx.fillRect(0, 0, dimensions.width, dimensions.height);
 
       // Draw text
       ctx.font = '54px serif';
       ctx.fillStyle = 'black';
       ctx.textAlign = 'center';
-      ctx.fillText(backgroundText, 1920/2, 1080/2);
+      ctx.fillText(backgroundText, dimensions.width/2, dimensions.height/2);
 
       // Create and complete download
       aRef.current.href = canvasRef.current.toDataURL("image/png");
@@ -69,7 +94,7 @@ function App() {
     return (
       <div>
         <button onClick={()=>setShouldDownload(true)}>Download</button>
-        <canvas ref={canvasRef} width={1920} height={1080} hidden/>
+        <canvas ref={canvasRef} width={dimensions.width} height={dimensions.height} hidden/>
         {// eslint-disable-next-line
         }<a ref={aRef} download="back.png" hidden></a>
       </div>
@@ -79,16 +104,18 @@ function App() {
   return (
     <div className="App" style={{textAlign: 'center'}}>
 
+
       <label>
         Input Text: 
         <input type="text" id="inputText" onChange={e => setBackgroundText(e.target.value)}>
         </input>
       </label>
+      <DimensionDropdown/>
       <DownloadBackground/>
       
       <div>
         <h2>Two-Thirds Scale Display</h2>
-        <DisplayCanvas width={1920} height={1080} font={font}/>
+        <DisplayCanvas font={font}/>
       </div>
     </div>
   );
